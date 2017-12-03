145a997fcf6a7cd8ddb4f4c27bbb46d53ca694f3
fuouo <fuouokun@gmail.com>
2017-12-04 02:52:20 +0800

    Animation System

---
+++ Assets/Camcorder.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Camcorder : MonoBehaviour {
+
+	public GameObject camCorder;
+	public Image batteryLife;
+	public Text Record;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (startCamera ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
+			GetComponent<Camera> ().fieldOfView--;
+		} else if (Input.GetAxis ("Mouse ScrollWheel") < 0)
+			GetComponent<Camera> ().fieldOfView++;
+	}
+
+	IEnumerator startCamera(){
+		StartCoroutine (startBattery ());
+		StartCoroutine (blinkRecord ());
+
+		yield return null;
+	}
+
+	IEnumerator blinkRecord(){
+		Record.enabled = !Record.enabled;
+		yield return new WaitForSeconds (0.5f);
+		StartCoroutine (blinkRecord ());
+	}
+
+	IEnumerator startBattery(){
+		if (batteryLife.fillAmount <= 0.5f) {
+			camCorder.GetComponent<Animator> ().SetBool ("isDead", true);
+			yield return new WaitForSeconds (1f);
+			yield return new WaitForEndOfFrame();
+			camCorder.GetComponent<Animator> ().SetBool ("isDead", false);
+			camCorder.gameObject.SetActive (false);
+
+
+		} else {
+			Debug.Log (batteryLife.fillAmount);
+			yield return new WaitForSeconds (1.0f);
+			batteryLife.fillAmount -= 0.10f;
+			StartCoroutine (startBattery ());
+		}
+	}
+}
---
+++ Assets/Character_Monster/Monster.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+
+	public Animator animator;
+	public bool isWalking = true;
+	public bool isHit = false;
+	public Quaternion startingRotation;
+	public float rotationSpeed;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		isWalking = true;
+
+		animator.SetBool ("isWalking", isWalking);
+		startingRotation = this.transform.rotation;
+		StartCoroutine (walk());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isWalking && !isHit)
+			Move ();
+		
+	}
+
+	IEnumerator walk(){
+		if (isHit)
+			yield return null;
+
+		yield return new WaitForSeconds (5.0f);
+		isWalking = !isWalking;
+		animator.SetBool ("isWalking", isWalking);
+		startingRotation = this.transform.rotation;
+		StartCoroutine (Rotate (-90)); //rotate during idle time
+		StartCoroutine (walk());
+
+	}
+
+	IEnumerator Rotate(float rotationAmount){
+		Quaternion finalRotation = Quaternion.Euler( 0, rotationAmount, 0 ) * startingRotation;
+
+		while(this.transform.rotation != finalRotation){
+			this.transform.rotation = Quaternion.Lerp(this.transform.rotation, finalRotation, Time.deltaTime*rotationSpeed);
+			yield return 0;
+		}
+	}
+		
+	void Move(){
+		transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+
+
+
+}
